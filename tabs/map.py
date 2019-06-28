65a17cf5b66f8f45e66ed462ce014fa767ded4ad
will haeck <will.haeck@gmail.com>
2019-06-28 11:44:01 -0700

    listit

--- tabs/map.py
+++ tabs/map.py
@@ -118,12 +118,14 @@ def extract_zip_display_graph(input_value, retirement_distance):
 def zoom_map_on_zip(input_value, retirement_distance):
     retirement_date = 2019 + retirement_distance
     data = []
+    projections = []
+
     center_lat = zip_lat_lng[zip_lat_lng['ZIP'] == input_value]['LAT'].values[0]
     center_lon = zip_lat_lng[zip_lat_lng['ZIP'] == input_value]['LNG'].values[0]
     for i in range(len(zip_lat_lng.LAT)):
         if (zip_lat_lng.iloc[i].LAT >= center_lat-1.0 and zip_lat_lng.iloc[i].LAT <= center_lat+1.0) and (zip_lat_lng.iloc[i].LNG >= center_lon - 1.0 and zip_lat_lng.iloc[i].LNG <= center_lon + 1.0):
-            zip_lat_lng.iloc[i]["PROJECTION"] = get_prediction_price(input_value, retirement_date)
-            print(zip_lat_lng.iloc[i]["PROJECTION"])
+            projections.append(get_prediction_price(input_value, retirement_date))
+    print(projections)
     data.append(
         Scattermapbox(
             lon=zip_lat_lng['LNG'].values,
