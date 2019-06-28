c6ed6b6ab35db575f0cafb23e2ec0b4fcea3f706
will haeck <will.haeck@gmail.com>
2019-06-28 11:16:05 -0700

    ugh, not &

--- tabs/map.py
+++ tabs/map.py
@@ -126,10 +126,10 @@ def zoom_map_on_zip(input_value):
     )
 
     for i in range(len(data[0].lat)):
-        print("Type of center: {}".format(type(center_lat)))
-        print("Type of market: {}".format(type(data[0].lat[i])))
-        # if (data[0].lat[i] >= center_lat-1 and data[0].lat[i] <= center_lat+1) and (data[0].lon[i] >= center_lon - 1 & data[0].lon[i] <= center_lon + 1):
-            # print("True")
+        # print("Type of center: {}".format(type(center_lat)))
+        # print("Type of market: {}".format(type(data[0].lat[i])))
+        if (data[0].lat[i] >= center_lat-1.0 and data[0].lat[i] <= center_lat+1.0) and (data[0].lon[i] >= center_lon - 1.0 and data[0].lon[i] <= center_lon + 1.0):
+            print("True")
         # print(data[0].lat[0])
         # if is_within_1_degree({'lat': center_lat, 'lon': center_lon}, {'lat': data[0].lat[i], 'lon': data[0].lon[i]}):
         #     print('True')
