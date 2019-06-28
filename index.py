1cacea4794ae0cef4ca2b3ed799d058117e02a99
will haeck <will.haeck@gmail.com>
2019-06-28 09:57:43 -0700

    added assets

--- index.py
+++ index.py
@@ -5,6 +5,8 @@ from dash.dependencies import Input, Output
 
 from app import app, server
 
+style = {'maxWidth': '960px', 'margin': 'auto'}
+
 app.layout = html.Div([
     dcc.Tabs(id='tabs', value='tab-intro', children=[
         dcc.Tab(label='Intro', value='tab-intro'),
