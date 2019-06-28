25dc2f12d479f2cb0bf8969005f69ebef17856d1
will haeck <will.haeck@gmail.com>
2019-06-28 11:32:03 -0700

    seeing what the df contains

--- tabs/map.py
+++ tabs/map.py
@@ -75,7 +75,8 @@ def get_prediction_price(zip_code, retirement_date):
     p_df = prophet_df_from_zillow_row(zip_code)
     forecast = pd.read_pickle('pickles/{}_forecast.pkl'.format(zip_code))
     forecast = forecast[forecast['ds'] < str(retirement_date)]
-    return forecast.tail(1)['y_hat']
+    print(forecast.tail(1))
+    # return forecast.tail(1)['y_hat']
 
 @app.callback(
     Output(component_id="retirement-label", component_property='children'),
