5022ba95e04337097728ff9a3df2c25bf02c1220
will haeck <will.haeck@gmail.com>
2019-06-28 09:05:42 -0700

    imported tabs

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+from tabs import intro, map
+
 app.layout = html.Div([
     dcc.Tabs(id='tabs', value='tab-intro', children=[
         dcc.Tab(label='Intro', value='tab-intro'),
