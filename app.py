7d5048925d7f221aa774214953ed8a1c0a482dd9
will haeck <will.haeck@gmail.com>
2019-06-28 09:34:05 -0700

    added favicon

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.config.suppress_callback_exceptions = True
+app.title = "Retirement Hunt"
 
 server = app.server
