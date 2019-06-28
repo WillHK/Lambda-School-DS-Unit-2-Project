736d7a426c99a5b9825e6ce5edf9508e963bfe37
will haeck <will.haeck@gmail.com>
2019-06-28 10:08:50 -0700

    minor adjustments

--- tabs/intro.py
+++ tabs/intro.py
@@ -4,16 +4,16 @@ import dash_html_components as html
 
 from app import app
 
-layout = [dcc.Markdown("""
-### Plan your perfect retirement
+layout = html.Div(children=[html.H6("Plan your perfect retirement"),
+            html.Img(src="/assets/beach_house.jpg"),
+            dcc.Markdown("""
+            The coming years will see a record number of Americans retire as the
+            Baby Boomer generation reaches their golden years. For many people
+            retirement involves finding a warm sunny place to enjoy their sunset years,
+            but as the cost of buying a house continues it's march upwards it becomes even
+            more important to find an affordable home.
 
-The coming years will see a record number of Americans retire as the
-Baby Boomer generation reaches their golden years. For many people
-retirement involves finding a warm sunny place to enjoy their sunset years,
-but as the cost of buying a house continues it's march upwards it becomes even
-more important to find an affordable home.
-
-Retirement Hunt allows you to search by Zip Code and easily find the projected
-average home value in the region. Take advantage of the internet age and find
-the home for your old age.
-""")]
+            Retirement Hunt allows you to search by Zip Code and easily find the projected
+            average home value in the region. Take advantage of the internet age and find
+            the home for your old age.
+            """)])
